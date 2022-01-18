ef0ab94671887c07505152da8c70e94b1f8dad42
Ramon Meffert <ramon.meffert@gmail.com>
2022-01-18 13:57:11 +0100

    Change word embedding - Move to native FastText package - Switch corpus from Lee (24kB) to common crawl/wikipedia (~1.2GB) - Extract common code to base class for DT and SVM taggers - Some cleanup

--- baseline/SvmTagger.py
+++ baseline/SvmTagger.py
@@ -6,12 +6,12 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from gensim.models.fasttext import FastText
-from gensim.test.utils import datapath
+from baseline.WordEmbeddingClassifier import WordEmbeddingClassifier
 
 import data
 import os
 import pickle
+
 
 @dataclass
 class SvmClassifierOptions:
@@ -29,41 +29,14 @@ class SvmClassifierOptions:
     load_pretrained = True
 
 
-class SvmClassifier:
+class SvmClassifier(WordEmbeddingClassifier):
     """A semantic tagger using a support-vector machine.
     """
     def __init__(self, options: SvmClassifierOptions = None) -> None:
-        self.__options = options or SvmClassifierOptions() # take defaults if no options given
+        super().__init__()
+        # take defaults if no options given
+        self.__options = options or SvmClassifierOptions()
         self.__model = None
-        self.__word_embedding = self.__get_word_embedding()
-
-    def __get_word_embedding(self) -> FastText:
-        path_name = './models/fasttext.mod'
-        if os.path.exists(path_name):
-            print("Found pre-trained word embedding")
-            return FastText.load(path_name)
-        else:
-            print("No pre-trained word embedding found, training now...")
-            # Training on Lee corpus
-            corpus_file = datapath('lee_background.cor')
-            model = FastText(vector_size=100)
-            model.build_vocab(corpus_file=corpus_file)
-            model.train(
-                corpus_file=corpus_file, epochs=model.epochs,
-                total_examples=model.corpus_count, total_words=model.corpus_total_words
-            )
-            print("Training finished. Saving model for next use...")
-            os.makedirs('./models/', exist_ok=True)
-            model.save(path_name, separately=[])
-            return model
-
-
-    def __prepare_data(self, input_data):
-        in_raw, out = data.dt_data(input_data)
-        # Prepare the data by transforming words to vectors
-        in_vectors = self.__word_embedding.wv[in_raw]
-        return in_raw, in_vectors, out
-
 
     def load_model(self):
         if os.path.exists('./models/svm_model.pkl'):
@@ -78,35 +51,31 @@ class SvmClassifier:
         """
         if self.__options.load_pretrained:
             if self.load_model():
-                print("Found pretrained svm model. Skipping training (use --force-train to force training).")
+                print(
+                    "Found pretrained svm model. Skipping training (use --force-train to force training)."
+                )
                 return
             else:
                 print("No pretrained svm model found, training now...")
 
-        _, data_in, data_out = self.__prepare_data(input_data)
-        in_train, in_test, out_train, out_test = train_test_split(data_in, data_out)
-
-
+        _, data_in, data_out = self.prepare_data(input_data)
+        in_train, in_test, out_train, out_test = train_test_split(
+            data_in, data_out)
 
         if self.__options.use_grid_search:
             params = {
-                # TODO: params 
-                'C' : [0.1, 1, 10, 100, 1000],
+                # TODO: params
+                'C': [0.1, 1, 10, 100, 1000],
             }
             clf = make_pipeline(
-                StandardScaler(), 
-                GridSearchCV(
-                    svm.SVC(kernel=self.__options.kernel), 
-                    params, 
-                    n_jobs=4,
-                    refit=True
-                    )
-                )
+                StandardScaler(),
+                GridSearchCV(svm.SVC(kernel=self.__options.kernel),
+                             params,
+                             n_jobs=4,
+                             refit=True))
         else:
-            clf = make_pipeline(
-                StandardScaler(), 
-                svm.SVC(kernel=self.__options.kernel)
-                )
+            clf = make_pipeline(StandardScaler(),
+                                svm.SVC(kernel=self.__options.kernel))
 
         clf.fit(X=in_train, y=out_train)
         self.__model = clf
@@ -121,18 +90,18 @@ class SvmClassifier:
         with open('./models/svm_model.pkl', 'wb') as model_pickle:
             pickle.dump(self.__model, model_pickle)
 
-
     def accuracy(self, input_path):
         """Evaluates the accuracy of the model on a (tagged) test set"""
         if self.__model is None:
             print("No model available. Please train the model first.")
         else:
-            words, data_vectors, true_tags = self.__prepare_data(input_path)
+            _, data_vectors, true_tags = self.prepare_data(input_path)
             predictions = self.__model.predict(data_vectors)
             acc = accuracy_score(true_tags, predictions)
             input_filename = os.path.basename(input_path)
-            print(f"This model has an accuracy of {acc * 100:02}% on {input_filename}.")
-
+            print(
+                f"This model has an accuracy of {acc * 100:02}% on {input_filename}."
+            )
 
     def classify(self, input_path) -> List:
         """Classify new data after training. Expects a list of words as input.
@@ -141,7 +110,7 @@ class SvmClassifier:
         if self.__model is None:
             print("No model available. Please train the model first.")
         else:
-            words, data_vectors, _ = self.__prepare_data(input_path)
+            words, data_vectors, _ = self.prepare_data(input_path)
             predictions = self.__model.predict(data_vectors)
             input_filename = os.path.basename(input_path)
             os.makedirs('./output/', exist_ok=True)
@@ -150,5 +119,5 @@ class SvmClassifier:
                 f.write("word\tpredicted tag\n")
                 for (d_input, d_output) in zip(words, predictions):
                     f.write(d_input + '\t' + d_output + '\n')
-            
+
             print(f'Output written to file {file_name}')
---
+++ baseline/WordEmbeddingClassifier.py
@@ -0,0 +1,40 @@
+import fasttext
+import fasttext.util
+import os
+import data
+
+class WordEmbeddingClassifier:
+    """Base class for the SVM and DT taggers since they use the same word
+    embedding
+    """
+
+    def __init__(self) -> None:
+        self.__word_embedding: fasttext.FastText = self.__get_word_embedding()
+
+    @classmethod
+    def __get_word_embedding(cls) -> fasttext.FastText:
+        """Get a pretrained word embedding. Downloads it first if necessary."""
+
+        # Save current wd
+        current_wd = os.getcwd()
+
+        # Download model to ./models/ subfolder
+        os.makedirs('./models/', exist_ok=True)
+        os.chdir('./models')
+        fasttext.util.download_model('en')
+        
+        # Change back to original wd
+        os.chdir(current_wd)
+
+        # Return model
+        return fasttext.load_model('./models/cc.en.300.bin')
+
+    def prepare_data(self, input_data):
+        """Transform a data set of words into vectors"""
+
+        in_raw, out = data.dt_data(input_data)
+        # Prepare the data by transforming words to vectors
+        # This is slow because FastText doesn't have a vectorized way to do this
+        # (i.e. we would ideally do self.__word_embedding[in_raw])
+        in_vectors = [self.__word_embedding[word] for word in in_raw]
+        return in_raw, in_vectors, out
